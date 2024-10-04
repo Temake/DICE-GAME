adff0daf704cc9e98b22026ee215af6da93311d2
Temake <130784362+Temake@users.noreply.github.com>
2024-10-04 04:13:56 +0100

    button features

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
-var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-var diceImages ="images/" + "dice" + randomNumber1 + ".png";
-var imag = document.querySelectorAll("img")[0].setAttribute("src",diceImages);
-var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-var diceImages2 ="images/" + "dice" + randomNumber2 + ".png";
-var imag2 = document.querySelectorAll("img")[1].setAttribute("src",diceImages2);
-if (randomNumber1==randomNumber2){
-   document.querySelector("h1").innerHTML= "It is a tie!";
-}
-else if(randomNumber1>randomNumber2){
-        document.querySelector("h1").innerHTML= "Player 1 Wins";
-}
-else {
-    document.querySelector("h1").innerHTML= "Player 2 Wins";
+var score1 = 0;
+var score2 = 0;
+
+function rollDice() {
+    var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+    var diceImage1 = "images/dice" + randomNumber1 + ".png";
+    document.querySelector(".img1").setAttribute("src", diceImage1);
+
+    var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+    var diceImage2 = "images/dice" + randomNumber2 + ".png";
+    document.querySelector(".img2").setAttribute("src", diceImage2);
+
+    if (randomNumber1 === randomNumber2) {
+        document.getElementById("result").innerHTML = "It's a Tie!";
+    } else if (randomNumber1 > randomNumber2) {
+        document.getElementById("result").innerHTML = "Player 1 Wins!";
+        score1++;
+    } else {
+        document.getElementById("result").innerHTML = "Player 2 Wins!";
+        score2++;
+    }
+
+    // Update the scores
+    document.getElementById("score1").innerHTML = "Score: " + score1;
+    document.getElementById("score2").innerHTML = "Score: " + score2;
 }
 
+// Add a click event listener to the "Roll Again" button
+document.getElementById("rollBtn").addEventListener("click", rollDice);
